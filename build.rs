49ffa933b3fa1acba7bfbc7f7cf8e4a4c7faec9f
g4mless <g4mless@proton.me>
2025-07-20 16:05:39 +0700

    add icon

--- build.rs
+++ build.rs
@@ -2,9 +2,7 @@
 fn main() {
     if cfg!(target_os = "windows") {
         let mut res = winres::WindowsResource::new();
-        // Menunjuk ke file resource script yang akan kita buat
         res.set_resource_file("app.rc");
-        // Mengkompilasi resource ke dalam aplikasi
         if let Err(e) = res.compile() {
             eprintln!("{}", e);
             std::process::exit(1);
